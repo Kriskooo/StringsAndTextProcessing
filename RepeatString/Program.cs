268f22ddebc156433f2603b87d1dde93a956b372
Kriskooo <kris.danielov@gmail.com>
2021-02-10 20:48:32 +0200

    StringsAndTextProcessing

---
+++ RepeatString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] word = Console.ReadLine().Split();
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int k = 0; k < word[i].Length; k++)
+                {
+                    Console.Write(word[i]);
+                }
+            }
+        }
+    }
+}
---
+++ TextFilter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
